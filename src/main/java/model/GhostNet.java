3a1ada957ba5e77c98b2f3fa09b1db70e1506dc6
TerekSW <tobias984@gmx.net>
2025-04-10 22:29:27 +0200

    GhostNetz Klasse

---
+++ src/main/java/model/GhostNet.java
@@ -0,0 +1,65 @@
+package main.java.model;
+
+import jakarta.persistence.*;
+
+//Entität für Geisternetz in Datenbank
+@Entity
+public class GhostNet {
+
+    @Id // Primärschlüssel
+    @GeneratedValue // automatische Generierung der ID
+    private long id;
+
+    private String coordinates; // Koordinaten des Netzes
+    private String size; // Größe des Netzes
+
+    @Enumerated(EnumType.STRING) // speichert Enum als String
+    private GhostNetStatus status; // aktueller Status des Netzes
+
+    @ManyToOne // Viele Netze können von einer Person gemeldet werden
+    private Person meldendePerson;
+
+    @ManyToOne // Viele Netze können von einer Person geborgen werden
+    private Person bergendePerson;
+
+    // Getter und Setter
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public GhostNetStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GhostNetStatus status) {
+        this.status = status;
+    }
+
+    public Person getMeldendPerson() {
+        return meldendePerson;
+    }
+
+    public void setMeldendPerson(Person meldendePerson) {
+        this.meldendePerson = meldendePerson;
+    }
+
+    public Person getBergendePerson() {
+        return bergendePerson;
+    }
+
+    public void setBergendePerson(Person bergendePerson) {
+        this.bergendePerson = bergendePerson;
+    }
+}
