4dcc37748f6f83f33abff8921be661692d9c4d6f
TerekSW <tobias984@gmx.net>
2025-04-10 23:51:08 +0200

    Must 1 Netz melden/erfassen

--- src/main/java/methods/GhostNetMethods.java
+++ src/main/java/methods/GhostNetMethods.java
@@ -3,6 +3,7 @@ package main.java.methods;
 import java.util.List;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import main.java.model.*;
 
 @ApplicationScoped
@@ -23,6 +24,36 @@ public class GhostNetMethods {
         return entityManager.createQuery("SELECT a FROM GhostNet a WHERE a.status = :status", GhostNet.class)
                 .setParameter("status", status)
                 .getResultList(); // Ergebnis als Liste
+    }
+
+    // MUST 1 Netz erfassen (auch anonym möglich)
+    @Transactional
+    public void meldeNetz(GhostNet net) {
+
+        if(net == null) {
+            throw new IllegalArgumentException("Bitte tragen Sie die Informationen des Netzes ein.");
+        }
+
+        net.setStatus(GhostNetStatus.GEMELDET); 
+
+        Person person = net.getMeldendPerson();
+
+        boolean anonym = false;
+
+        if (person == null) { // Wenn keine Person angegeben ist, dann anonym
+            anonym = true;
+        } else if ((person.getName() == null || person.getName().isBlank()) &&
+                (person.getNumber() == null || person.getNumber().isBlank())) {
+            anonym = true;
+        }
+
+        if(anonym) {
+            person.setName(null);
+            person.setNumber(null);
+        }
+
+        net.setMeldendPerson(person);
+        entityManager.persist(net);
     }
 
     
